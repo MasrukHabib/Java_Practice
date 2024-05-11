7c18ee84f4c6f710eae134e3f0549a0adbf69d4a
Masruk Habib <masrukjim@gmail.com>
2024-05-11 11:17:35 +0530

    Update lab1_c.java remove name and add one line

--- Lab1,2/src/lab1_c.java
+++ Lab1,2/src/lab1_c.java
@@ -23,7 +23,7 @@ public class lab1_c {
         } else {
             System.out.println(num + " is not a prime number.");
         }
-        System.out.println("\n name:Lena Akter \n Enrollment: 92301703076 \n Section:2EC1 \n lab-batch: C");
+        System.out.println("\n name: \n Enrollment:  \n Section:2EC1 \n lab-batch: ");
         scanner.close();
     }
 }
