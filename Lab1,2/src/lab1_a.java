7dcee77c769bbfd6a82258a37808a98f1a6b99e3
Masruk Habib <masrukjim@gmail.com>
2024-05-11 11:16:49 +0530

    Update lab1_a.java remove names

--- Lab1,2/src/lab1_a.java
+++ Lab1,2/src/lab1_a.java
@@ -12,7 +12,7 @@ public class lab1_a {
         }
 
         System.out.println("Sum of first " + n + " natural numbers: " + sum);
-        System.out.println("\n name:Lena Akter \n Enrollment: 92301703076 \n Section:2EC1 \n lab-batch: C");
+        System.out.println("\n name: \n Enrollment:  \n Section: \n lab-batch: ");
         scanner.close();
     }
 }
