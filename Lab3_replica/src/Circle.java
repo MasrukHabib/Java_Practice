bf0e006e01565145f0b39fac901f7448c161474a
Masruk Habib <masrukjim@gmail.com>
2024-05-11 11:37:24 +0530

    Update Circle.java

--- Lab3_replica/src/Circle.java
+++ Lab3_replica/src/Circle.java
@@ -27,7 +27,7 @@ public class Circle {
 
         // Calculate and display circumference
         System.out.println("Circumference of circle: " + circle.calculateCircumference());
-        System.out.println("\n name:Lena Akter \n Enrollment: 92301703076 \n Section:2EC1 \n lab-batch: C");
+        System.out.println("\n name: \n Enrollment:  \n Section: \n lab-batch: ")
 
     }
 }
