7248f36b328fd07d2022f0103ae1b8fd9b22d98b
Masruk Habib <masrukjim@gmail.com>
2024-05-11 11:38:33 +0530

    Update Book.java

--- Lab5_Replica/src/Book.java
+++ Lab5_Replica/src/Book.java
@@ -30,6 +30,6 @@ public class Book {
         System.out.println();
         System.out.println("Data for b2:");
         b2.printData();
-        System.out.println("\n name:Lena Akter \n Enrollment: 92301703076 \n Section:2EC1 \n lab-batch: C");
+        System.out.println("\n name: \n Enrollment:  \n Section: \n lab-batch: ")
     }
 }
