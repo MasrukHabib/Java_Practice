cdb88d3c5a021201fdd9e3f6139ef36f7e67240c
Masruk Habib <masrukjim@gmail.com>
2024-09-02 00:43:30 +0530

    Update lab1_b.java

--- Lab1,2/src/lab1_b.java
+++ Lab1,2/src/lab1_b.java
@@ -7,9 +7,7 @@ public class lab1_b {
         double base = scanner.nextDouble();
         System.out.print("Enter the height of the triangle: ");
         double height = scanner.nextDouble();
-
         double area = 0.5 * base * height;
-
         System.out.println("Area of the triangle: " + area);
         System.out.println("\n name:Masruk Habib \n Enrollment:  \n Section: \n lab-batch: ");
         scanner.close();
